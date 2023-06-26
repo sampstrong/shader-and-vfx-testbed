aa26e652671e6fb90c536616483af5f96be53dfe
sampstrong <sampstrong@outlook.com>
2023-06-26 10:56:58 -0400

    created scene for audio pulse

---
+++ Assets/2_VFXTestbed/AudioPulse/Scripts/BoxFrameRotation.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxFrameRotation : MonoBehaviour
+{
+    private enum RotDirection
+    {
+        xPos = 0,
+        xNeg = 1,
+        yPos = 2,
+        yNeg = 3,
+        zPos = 4,
+        zNeg = 5
+    }
+
+    private RotDirection _rotDirection;
+
+    [SerializeField] private float _speed = 20f;
+    [SerializeField] private float _rotTimer = 0f;
+    [SerializeField] private float _rotInterval = 2f;
+    [SerializeField] private float _rotDuration = 0.5f;
+        
+    void Update()
+    {
+        ContinuouslyRotate();
+        
+        _rotTimer += Time.deltaTime;
+        if (_rotTimer >= _rotInterval)
+        {
+            var newRot = GetRandomRotation();
+            StartCoroutine(LerpToNewRotation(transform.rotation, newRot));
+            _rotTimer = 0f;
+        }
+    }
+
+    private void ContinuouslyRotate()
+    {
+        transform.Rotate(Vector3.up, _speed * Time.deltaTime);
+        transform.Rotate(Vector3.right, _speed / 3 * Time.deltaTime);
+    }
+
+    private Quaternion GetRandomRotation()
+    {
+        int directionIndex = Random.Range(0, 6);
+
+        _rotDirection = (RotDirection)directionIndex;
+
+        Quaternion currentRot = transform.rotation;
+        Quaternion newRot = Quaternion.identity;
+
+        switch (_rotDirection)
+        {
+            case RotDirection.xNeg:
+                newRot = currentRot * Quaternion.Euler(-90, 0, 0);
+                break;
+            case RotDirection.xPos:
+                newRot = currentRot * Quaternion.Euler(90, 0, 0);
+                break;
+            case RotDirection.yNeg:
+                newRot = currentRot * Quaternion.Euler(0, -90, 0);
+                break;
+            case RotDirection.yPos:
+                newRot = currentRot * Quaternion.Euler(0, 90, 0);
+                break;
+            case RotDirection.zNeg:
+                newRot = currentRot * Quaternion.Euler(0, 0, -90);
+                break;
+            case RotDirection.zPos:
+                newRot = currentRot * Quaternion.Euler(0, 0, 90);
+                break;
+        }
+
+        return newRot;
+    }
+
+    private IEnumerator LerpToNewRotation(Quaternion startRot, Quaternion endRot)
+    {
+        for (float t = 0; t < _rotDuration; t += Time.deltaTime)
+        {
+            var rot = Quaternion.Slerp(startRot, endRot, t / _rotDuration);
+            transform.rotation = rot;
+
+            yield return null;
+        }
+
+        transform.rotation = endRot;
+    }
+}
