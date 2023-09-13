bd23d244941a5125f14fcfe1d2d70e7eab4a95f9
sampstrong <sampstrong@outlook.com>
2023-09-12 20:45:32 -0400

    update frame counter

--- Assets/1_ShaderTestbed/Compute/Instancing/Scripts/FrameRateCounter.cs
+++ Assets/1_ShaderTestbed/Compute/Instancing/Scripts/FrameRateCounter.cs
@@ -4,22 +4,48 @@ using UnityEngine.Experimental.Audio;
 
 public class FrameRateCounter : MonoBehaviour
 {
+    private enum DisplayMode { FPS, MS }
+
+    [SerializeField] private DisplayMode displayMode = DisplayMode.FPS;
     [SerializeField] private TextMeshProUGUI display;
     [SerializeField, Range(0.1f, 2f)] private float sampleDuration = 1f;
 
     private int frames;
-    private float duration;
+    private float duration, bestDuration = float.MaxValue, worstDuration;
 
     private void Update()
     {
         float frameDuration = Time.unscaledDeltaTime;
         frames += 1;
         duration += frameDuration;
+
+        if (frameDuration < bestDuration) {bestDuration = frameDuration;}
+        if (frameDuration > worstDuration) {worstDuration = frameDuration;}
+
         if (duration >= sampleDuration)
         {
-            display.SetText($"FPS\n{frames / duration:0}\n000\n000");
+            if (displayMode == DisplayMode.FPS)
+            {
+                display.SetText(
+                    "FPS\n{0:0}\n{1:0}\n{2:0}",
+                    1f / bestDuration,
+                    frames / duration,
+                    1f / worstDuration
+                );
+            }
+            else
+            {
+                display.SetText(
+                    "MS\n{0:1}\n{1:1}\n{2:1}",
+                    1000f * bestDuration,
+                    1000f * duration / frames,
+                    1000f * worstDuration
+                );
+            }
             frames = 0;
             duration = 0f;
+            bestDuration = float.MaxValue;
+            worstDuration = 0f;
         }
     }
 }
